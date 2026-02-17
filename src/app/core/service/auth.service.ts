913b0b9c7f2deca1581211dcac28a5be51a790c0
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2026-02-17 17:32:44 +0000

    feat(auth): création du service AuthService avec injection de HttpClient

--- src/app/core/service/auth.service.ts
+++ src/app/core/service/auth.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {User} from '../models/user.model';
+import {Observable ,map} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+ private http= inject(HttpClient);
+ private apiUrl= 'http://localhost:3000/users';
 }
+
+
+
+
+
+
+
+
+
+  }
+
