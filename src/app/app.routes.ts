7c1a69c4562b0f98fed6e46640252d315fe9062f
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2026-02-18 20:50:01 +0000

    feat(register): create standalone RegisterComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
 import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  {path: 'register', component: RegisterComponent},
   {path: 'home' ,component: HomeComponent},
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
---
+++ src/app/features/auth/register/register.component.ts
@@ -0,0 +1,19 @@
+import { Component ,inject} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import{ AuthService } from '../../../core/service/auth.service';
+
+@Component({
+  selector: 'app-register',
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  standalone: true,
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+
+
+
+}
