5012f565e3bbd31e1579986dcb18c551faa95298
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2026-02-20 21:13:06 +0000

    feat(job-service): injection de HttpClient avec la fonction inject()

---
+++ src/app/core/services/job.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, inject } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable, map} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JobService {
+
+private http = inject(HttpClient);
+
+
+
+
+
+}
