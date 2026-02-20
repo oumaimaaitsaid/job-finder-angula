83cb885595b73dc06719189d34e6c324532ccf4a
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2026-02-20 21:10:05 +0000

    feat(home): ajout des états jobs, filteredJobs, searchTerm et loading

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { JobService } from '../../core/services/job.service';
+import { JobCardComponent } from '../jobs/job-card/job-card.component';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  standalone: true,
+  imports: [CommonModule, JobCardComponent, FormsModule],
+  templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private jobService = inject(JobService);
+  private router = inject(Router);
+
+  jobs: any[] = [];
+  filteredJobs: any[] = [];
+  searchTerm: string = '';
+  loading: boolean = true;
+
+
+
+
+
+
 
 }
